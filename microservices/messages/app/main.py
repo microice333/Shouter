b11f92a3a4e5a0b95da3b56f90b2a65d0d7c0f5a
Dalak <dalak.bartek@gmail.com>
2018-06-12 17:27:59 +0200

    Messages index

--- microservices/messages/app/main.py
+++ microservices/messages/app/main.py
@@ -11,11 +11,13 @@ app = Flask(__name__)
 def all_messages(username):
     messages = []
 
-    for k in redis.keys(pattern='messages/*'):
+    for k in redis.keys(pattern='messages_u/*'):
         author = k.split('/')[1]
-        for message in redis.lrange(f"messages/{author}", 0, -1):
-            liked = username in redis.smembers(f"likes/{author}/{message}")
-            messages.append({'message': message, 'author': author, 'liked': liked})
+
+        for idx in redis.lrange(f"messages_u/{author}", 0, -1):
+            liked = username in redis.smembers(f"likes/{idx}")
+            message = redis.get(f"messages/{idx}")
+            messages.append({'message': message, 'author': author, 'liked': liked, 'id': idx})
 
     return jsonify({'messages': messages})
 
@@ -23,31 +25,35 @@ def all_messages(username):
 @app.route("/messages/<username>", methods=['GET', 'PUT'])
 def user_messages(username):
     if request.method == 'GET':
-        messages = redis.lrange(f"messages/{username}", 0, -1)
+        messages = redis.lrange(f"messages_u/{username}", 0, -1)
 
         return jsonify({'messages': len(messages)})
     elif request.method == 'PUT':
         message = request.get_json()['message']
+        idx = redis.incr("messages/id")
+        redis.lpush(f"messages_u/{username}", idx)
+        redis.set(f"messages/{idx}", message)
+        redis.set(f"authors/{idx}", username)
 
-        redis.lpush(f"messages/{username}", message)
-        messages = redis.lrange(f"messages/{username}", 0, -1)
-
-        return jsonify({'messages': messages})
+        return jsonify({'id': idx})
 
 
 @app.route("/like/<username>", methods=['PUT', 'DELETE'])
 def like(username):
-    author = request.get_json()['author']
-    message = request.get_json()['message']
+    idx = request.get_json()['idx']
 
     if request.method == 'PUT':
-        redis.sadd(f"likes/{author}/{message}", username)
+        redis.sadd(f"likes/{idx}", username)
+        message = redis.get(f"messages/{idx}")
+        author = redis.get(f"authors/{idx}")
 
-        return jsonify({'message': message, 'author': author, 'liked': True})
+        return jsonify({'message': message, 'author': author, 'liked': True, 'id': idx})
     elif request.method == 'DELETE':
-        redis.srem(f"likes/{author}/{message}", username)
+        redis.srem(f"likes/{idx}", username)
+        message = redis.get(f"messages/{idx}")
+        author = redis.get(f"authors/{idx}")
 
-        return jsonify({'message': message, 'author': author, 'liked': False})
+        return jsonify({'message': message, 'author': author, 'liked': False, 'id': idx})
 
 
 if __name__ == "__main__":
